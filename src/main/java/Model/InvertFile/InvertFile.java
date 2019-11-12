12d16103bc3d6a2827350ba6745651b633e10b6f
MrBahur <MrBahur25@gmail.com>
2019-11-12 13:21:58 +0200

    added InvertFile package

---
+++ src/main/java/Model/InvertFile/InvertFile.java
@@ -0,0 +1,30 @@
+package Model.InvertFile;
+
+import Model.File.Phrase;
+
+import java.util.HashMap;
+
+public class InvertFile {
+    private int[][] matrix;
+    private MyDictionary words;
+    private HashMap<String,Integer> documents;
+    private int currentSizeRows;
+    private int currentSizeColumns;
+
+    private void extendRow(){
+
+    }
+
+    private void extendColumn(){
+
+    }
+
+    public void addWord(Phrase p, String doc){
+
+    }
+
+    public void addDoc(String doc){
+
+    }
+
+}
---
+++ src/main/java/Model/InvertFile/MyDictionary.java
@@ -0,0 +1,15 @@
+package Model.InvertFile;
+
+import Model.File.Phrase;
+import javafx.util.Pair;
+
+import java.util.HashMap;
+
+public class MyDictionary {
+    private int nextPhrase;
+    private HashMap<Phrase, Pair<Integer,Integer>> wordsList;
+
+    public void insertWord(Phrase p){
+
+    }
+}
