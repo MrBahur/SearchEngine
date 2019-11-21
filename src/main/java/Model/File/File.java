a5de57a83c8230e08260a962ad62bbe757e78451
MrBahur <MrBahur25@gmail.com>
2019-11-21 16:06:56 +0200

    Revert "change name, added file"
    This reverts commit 13fe9e9cd3bec3f67ee809901c09ac546e8faf8d.
    
    # Conflicts:
    #	src/main/java/Model/File/File.java
    #	src/main/java/Model/ReadFile/ReadFile.java

---
+++ src/main/java/Model/File/File.java
@@ -0,0 +1,35 @@
+package Model.File;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class File implements Iterable<Document> {
+    private String fileName;
+    private FileIterator<Document> iterator;
+    private ArrayList<Document> documents;
+
+    //TODO Implement Iterator of Documents.
+    @Override
+    public Iterator<Document> iterator() {
+        return iterator;
+    }
+
+
+    private class FileIterator<Document> implements Iterator<Document>{
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Document next() {
+            return null;
+        }
+    }
+    //TODO Implement Test for File.
+    //test for File
+    public static void main(String[] args) {
+
+    }
+}
