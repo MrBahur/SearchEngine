983a198deec9bbcafa3558e6b624cadbf98f9a37
Shaharbsh <shaBSH23>
2019-11-21 10:09:53 +0200

    Number toString

--- src/main/java/Model/File/Number.java
+++ src/main/java/Model/File/Number.java
@@ -1,31 +1,39 @@
 package Model.File;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Number extends Phrase {
     private double value;
     private String sign;
 
-    public Number (double value) {
+ /*   public Number (double value) {
         this.sign = null;
         this.value = value;
-    }
+    }*/
     @Override
     public String toString() {
         String str = Double.toString(this.value);
         double num = this.value;
-        if (this.value>999 && this.value<100000) {
+        if (this.value>999 && this.value<1000000) {
             num = num/1000;
-            str = Double.toString(num) + "K";
+            DecimalFormat d = new DecimalFormat("###.###");
+            d.setRoundingMode(RoundingMode.DOWN);
+            str = d.format(num) + "k";
         }
         if (this.value>999999 && this.value<1000000000) {
             num = num/1000000;
-            str = Double.toString(num) + "M";
+            DecimalFormat d = new DecimalFormat("###.###");
+            d.setRoundingMode(RoundingMode.DOWN);
+            str = d.format(num) + "M";
         }
         if (this.value>999999999) {
             num = num/1000000000;
-            str = Double.toString(num) + "B";
+            DecimalFormat d = new DecimalFormat("###.###");
+            d.setRoundingMode(RoundingMode.DOWN);
+            str = d.format(num) + "B";
         }
         System.out.println(str);
-
         return str;
     }
 
