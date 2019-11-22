85c509636de82922a12d37ff183a142c7774fcc8
MrBahur <MrBahur25@gmail.com>
2019-11-22 09:55:10 +0200

    added Iterator for ReadFile

--- src/main/java/Model/ReadFile/ReadFile.java
+++ src/main/java/Model/ReadFile/ReadFile.java
@@ -3,27 +3,55 @@ package Model.ReadFile;
 import Model.File.MyFile;
 
 import java.io.File;
+import java.util.Iterator;
 
-public class ReadFile {
+public class ReadFile<T> implements Iterable<T> {
     private String path;
     private String[] directories;
     private int currentDir;
+    private StringIterator<String> iterator;
+
+    private class StringIterator<E> implements Iterator<E> {
+        private E[] list;
+
+        @Override
+        public boolean hasNext() {
+            return currentDir < list.length;
+        }
+
+        @Override
+        public E next() {
+            int x;
+            synchronized (this) {
+                x = currentDir++;
+            }
+            return list[x];
+        }
+    }
+
+    @Override
+    public Iterator iterator() {
+        return this.iterator;
+    }
 
     public ReadFile(String path) {
         this.path = path;
         directories = new File(path).list();
         currentDir = 0;
+        iterator = new StringIterator<>();
+        iterator.list = directories;
     }
 
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        ReadFile r = new ReadFile("F:\\Study\\SearchEngine\\corpus");
-        for (String s : r.directories) {
+        ReadFile<String> r = new ReadFile<>("F:\\Study\\SearchEngine\\corpus");
+        for (String s : r) {
             MyFile f = new MyFile(r.path + "\\" + s + "\\" + s);
-            //f.printFile();
         }
         long finish = System.currentTimeMillis();
         System.out.println("Time Elapsed =" + ((finish - start) / 1000.0) + "seconds");
     }
+
+
 }
