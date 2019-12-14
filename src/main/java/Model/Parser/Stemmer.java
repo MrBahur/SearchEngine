5535f81e7425c294d9b7bca7486cba3da6d38d82
MrBahur <MrBahur25@gmail.com>
2019-12-14 18:34:38 +0200

    added stemmer and stemmer dependency

--- src/main/java/Model/Parser/Stemmer.java
+++ src/main/java/Model/Parser/Stemmer.java
@@ -1,4 +1,21 @@
 package Model.Parser;
 
+import opennlp.tools.stemmer.PorterStemmer;
+
 public class Stemmer {
+    public static PorterStemmer p = new PorterStemmer();
+
+    public static String stem(String s) {
+        return p.stem(s);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Stemmer.stem("ASDASD"));
+        System.out.println(Stemmer.stem("Stemmer"));
+        System.out.println(Stemmer.stem("implementing"));
+        System.out.println(Stemmer.stem("the"));
+        System.out.println(Stemmer.stem("Porte"));
+        System.out.println(Stemmer.stem("Stemming"));
+        System.out.println(Stemmer.stem("Algorithm"));
+    }
 }
